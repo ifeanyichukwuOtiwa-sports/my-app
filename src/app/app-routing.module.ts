7538ee8507835f0c2fbc7d30f5b84b3ce5fbf1c2
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-10-22 13:07:09 +0100

    Step 2, Starting Rxjs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RxjsLearningComponent } from '@app/rxjs-learning/rxjs-learning.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'rxjs-learning', component: RxjsLearningComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
